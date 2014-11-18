f8bf46b23bc461975dfa63f70011cc5da1e79769
vvo <vincent@zeroload.net>
2014-11-18 16:08:00 +0100

    show stacks in development to the browser/client

--- middlewares/error.js
+++ middlewares/error.js
@@ -2,13 +2,15 @@
 // detects error handlers middlewares with function arity
 /*eslint no-unused-vars: 0*/
 
+var config = require('../config/');
+
 module.exports = function errorMiddleware(err, req, res, next) {
   var log = req.log;
 
   res.status(err.status || 500);
   res.render('error', {
       message: err.message,
-      error: {}
+      error: config.environment === 'development' ? err : {}
   });
 
   log.error(err, err.message);
